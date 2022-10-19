775388a7d12dbba9020a257e8d2b99a3df129ea5
Akhtar <akhtarcdgl@gmail.com>
2022-10-19 16:37:40 +0530

    The Prime and Palindrom Code is coded Kindly check it

---
+++ Palindrome.js
@@ -0,0 +1,12 @@
+let name = "namal";
+
+bag = "";
+
+for ( let i = 5 ; i >= 0 ; i--) {
+    bag = bag + name[i]
+    
+} if ( bag = name ){
+    console.log("Yes, Its a Palindrome");
+}else {
+    console.log("No, Its a Palindrome")
+}
---
+++ prime.js
@@ -0,0 +1,11 @@
+let number = 17
+
+if ( num%1===0)
+    { 
+        if (num%num===0)
+        
+        console.log("Yes");
+    }else{
+        console.log("No");
+    }
+    
